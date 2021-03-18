8d39113d0b5b295bfb00208dd3d6627ce366db25
Alejandra Otazu <mharale21@gmail.com>
2021-03-17 21:11:00 -0300

    Arreglo de html

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,5 +1,5 @@
   <!-- Footer -->
-  <footer class="py-5 bg-navy"
+  <footer class="py-5 bg-navy">
     <div class="container">
       <p class="m-0 text-center text-white">Copyright &copy; 2021 CAMMM</p>
     </div>
