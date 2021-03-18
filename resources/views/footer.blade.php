9ed75bef7e40eeecb00fc18cd78b58763cb75ce0
Alejandra Otazu <mharale21@gmail.com>
2021-03-18 13:19:00 -0300

    Cambios en el footer

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,7 +1,7 @@
   <!-- Footer -->
   <footer class="py-5 bg-navy">
     <div class="container">
-      <p class="m-0 text-center text-white">Copyright &copy; 2021 CAMMM</p>
+      <p class="m-0 text-center text-white">Copyright &copy; 2021 CAMM</p>
     </div>
     <!-- /.container -->
   </footer>
